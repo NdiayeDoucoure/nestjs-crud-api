149fa421b232a032093a1d7939a1219ccbb51e93
NdiayeDoucoure <doucoure.papa10@gmail.com>
2025-03-03 05:58:02 +0000

    feat: integrate Swagger for API documentation and enhance ProductDto with API properties

--- src/products/product.dto.ts
+++ src/products/product.dto.ts
@@ -1,4 +1,16 @@
+import { ApiProperty } from '@nestjs/swagger';
+
 export class ProductDto {
+  @ApiProperty({
+    title: 'The title of a product',
+    required: true,
+    type: 'string',
+  })
   readonly title: string;
+  @ApiProperty({
+    title: 'The description of a product',
+    required: false,
+    type: 'string',
+  })
   readonly description: string;
 }
--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -10,12 +10,15 @@ import {
 import { ProductsService } from './products.service';
 import { Product } from './product.entity';
 import { ProductDto } from './product.dto';
+import { ApiResponse } from '@nestjs/swagger';
 
 @Controller('products')
 export class ProductsController {
   constructor(private readonly productsService: ProductsService) {}
 
   @Get()
+  @ApiResponse({ status: 200, description: 'Return all products.' })
+  @ApiResponse({ status: 404, description: 'Not found.' })
   findAll(): Promise<Product[]> {
     return this.productsService.findAll();
   }
